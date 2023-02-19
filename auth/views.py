4c8cf75856cf73645e414f1b456383b6842f950e
katelocate <katelocat3@gmail.com>
2023-02-19 17:28:20 +0100

    Adds LoginView to the auth app views.

--- auth/views.py
+++ auth/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from rest_framework import permissions, status
+from rest_framework import views
+from rest_framework.response import Response
 
-# Create your views here.
+from . import serializers
+
+
+class LoginView(views.APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        serializer = serializers.LoginSerializer(
+            data=self.request.data,
+            context={'request': self.request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response(None, status=status.HTTP_202_ACCEPTED)
