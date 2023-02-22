fb45e682bfa2be4efe0aaa3f9fa0377ed3af756c
katelocate <katelocat3@gmail.com>
2023-02-22 17:17:13 +0100

    Includes chat_auth urls into the chat urls.

--- chat/chat/urls.py
+++ chat/chat/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
-import auth
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth/', include(auth.urls)),
+    path('auth/', include('chat_auth.urls')),
 ]
